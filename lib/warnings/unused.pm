9684741f970fed6e89fccdf13820ae95aa5ebf50
Fuji, Goro <g.psy.va@gmail.com>
2013-05-05 22:22:02 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 8f773c5..91905f5 100644
    --- a/Changes
    +++ b/Changes
    @@ -3,8 +3,8 @@ Revision history for warnings::unused
     TODO:
     	* to be based on PL_peepp?
    
    -NEXT
    -	- Deprecate this module. Use Test::Vars instead.
    +0.05 2013-05-05 22:16:22
    +    - Restore this module to CPAN again (github issue #1)
    
     0.04 Sun Mar  1 11:29:06 2009
     	- fix RT #43716, reported by Kevin Ryde

--- lib/warnings/unused.pm
+++ lib/warnings/unused.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 
 use strict;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
@@ -30,7 +30,7 @@ warnings::unused - Produces warnings when unused variables are detected
 
 =head1 VERSION
 
-This document describes warnings::unused version 0.04.
+This document describes warnings::unused version 0.05.
 
 =head1 SYNOPSIS
 
